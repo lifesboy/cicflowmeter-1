c3601851219142cc7ad97ee648598f021bcc0021
lifesboy <lifesboy@yahoo.com>
2022-02-20 22:32:24 +0700

    update package metadata

--- setup.py
+++ setup.py
@@ -6,9 +6,9 @@ import os
 from setuptools import find_packages, setup
 
 # Package meta-data.
-NAME = "cicflowmeter"
+NAME = "cicflowmeterd"
 DESCRIPTION = "CICFlowMeter V3 Python Implementation"
-URL = "https://github.com/lifesboy/cicflowmeter-1.git"
+URL = "https://github.com/lifesboy/cicflowmeter-d.git"
 EMAIL = "hieulw99@gmail.com"
 AUTHOR = "Le Hieu"
 REQUIRES_PYTHON = ">=3.7.0"
