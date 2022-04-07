77c49aa3569c5e46844bc40eaf75f45cb18e4f80
lifesboy <lifesboy@yahoo.com>
2022-04-07 22:26:35 +0700

    set all log default level is DEBUG

--- src/cicflowmeter/util/logger.py
+++ src/cicflowmeter/util/logger.py
@@ -5,19 +5,19 @@ _log_format = f"%(asctime)s - [%(levelname)s] - %(name)s - (%(filename)s).%(func
 
 def get_file_handler():
     file_handler = logging.FileHandler("/var/log/cicflowmeter/cicflowmeter.log")
-    file_handler.setLevel(logging.WARNING)
+    file_handler.setLevel(logging.DEBUG)
     file_handler.setFormatter(logging.Formatter(_log_format))
     return file_handler
 
 
 def get_stream_handler():
     stream_handler = logging.StreamHandler()
-    stream_handler.setLevel(logging.INFO)
+    stream_handler.setLevel(logging.DEBUG)
     stream_handler.setFormatter(logging.Formatter(_log_format))
     return stream_handler
 
 
-def get_logger(name: str, level: int = logging.INFO):
+def get_logger(name: str, level: int = logging.DEBUG):
     logger = logging.getLogger(name)
     logger.setLevel(level)
     logger.addHandler(get_file_handler())
