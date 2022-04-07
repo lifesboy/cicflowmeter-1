e122e3033142c8b2013ee93673acae98edb7c921
lifesboy <lifesboy@yahoo.com>
2022-04-07 22:32:26 +0700

    add error file

--- src/cicflowmeter/util/logger.py
+++ src/cicflowmeter/util/logger.py
@@ -1,11 +1,21 @@
 import logging
 
+LOG_INFO_FILE = '/var/log/cicflowmeter/cicflowmeter.log'
+LOG_ERROR_FILE = '/var/log/cicflowmeter/error.log'
+
 _log_format = f"%(asctime)s - [%(levelname)s] - %(name)s - (%(filename)s).%(funcName)s(%(lineno)d) - %(message)s"
 
 
 def get_file_handler():
-    file_handler = logging.FileHandler("/var/log/cicflowmeter/cicflowmeter.log")
+    file_handler = logging.FileHandler(LOG_INFO_FILE)
     file_handler.setLevel(logging.DEBUG)
+    file_handler.setFormatter(logging.Formatter(_log_format))
+    return file_handler
+
+
+def get_file_handler_error():
+    file_handler = logging.FileHandler(LOG_ERROR_FILE)
+    file_handler.setLevel(logging.ERROR)
     file_handler.setFormatter(logging.Formatter(_log_format))
     return file_handler
 
@@ -21,5 +31,6 @@ def get_logger(name: str, level: int = logging.DEBUG):
     logger = logging.getLogger(name)
     logger.setLevel(level)
     logger.addHandler(get_file_handler())
+    logger.addHandler(get_file_handler_error())
     logger.addHandler(get_stream_handler())
     return logger
