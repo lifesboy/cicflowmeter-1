f19850bf5c55d53a6fc4d0a7aa25581060349ebf
lifesboy <lifesboy@yahoo.com>
2022-12-27 21:01:31 +0700

    update test of fourier transform

--- tests/test_fastfourier-transform.py
+++ tests/test_fastfourier-transform.py
@@ -1,8 +1,12 @@
+import numpy as np
 from scipy.fft import fft
 
 
-arr = bytearray(0) + bytearray(100)
+arr = bytearray(0) + bytearray('12531526236236236', 'utf-8')
 
+N = 100
 y_arr = fft(arr)
+res = 2.0/N * np.abs(y_arr[0:N//2])
 
-print('y_arr=', y_arr[:10])
+print('y_arr=', y_arr)
+print('res=', res)
